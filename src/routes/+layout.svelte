f243c8464910ddcc20c239b04a17ef635c7c049a
Alberto Ginel <aginelsalvador@gmail.com>
2023-07-30 14:08:54 +0300

    funciona!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,14 +16,14 @@
 	<meta property="og:title" content="FakeGame" />
 	<meta
 		property="og:description"
-		content="Dominion 3:03:00 am 31/07/23 Monday in 13 hours ⌛1:00 👥 0/2"
+		content="Dominion\n3:03:00 am\nMonday 31/07/23\n⌛1:00 in 13 hours\n👥 0/2"
 	/>
 	<meta
 		property="og:image"
 		content="https://firebasestorage.googleapis.com/v0/b/undeadontime.appspot.com/o/gamesImages%2F15263e0e-45b3-4d26-b8ee-6d6cfbc588c5.png?alt=media&token=745cbeec-2dcc-4719-b7cf-0a987363dff0"
 	/>
-	<meta property="og:url" content="Your URL Here" />
-	<meta property="og:site_name" content="Your Site Name Here" />
+	<meta property="og:url" content="https://kit.svelte.dev/docs/adapters" />
+	<meta property="og:site_name" content="Undead On Time" />
 	<meta property="og:type" content="website" />
 </svelte:head>
 
