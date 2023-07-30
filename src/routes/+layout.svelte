029f6205b838ee4125cce7cb8d2cae8976f8e919
Alberto Ginel <aginelsalvador@gmail.com>
2023-07-30 13:21:46 +0300

    tarda mucho??

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,14 +14,6 @@
 <!-- svelte-ignore a11y-img-redundant-alt -->
 <svelte:head>
 	<title>Game Board</title>
-	<link rel="icon" href="https://fav.farm/🔥" />
-	<meta
-		property="og:image"
-		content="https://cdnb.artstation.com/p/assets/images/images/056/598/487/large/blanca-maeso-793c6688-f8b1-4a65-9d57-c6294bc08a1f.jpg?1669657042"
-	/>
-	<meta property="og:image:width" content="1200" />
-	<meta property="og:image:height" content="630" />
-
 	<meta property="og:title" content="La pagina!" />
 	<meta property="og:site_name" content="Ven por aqui" />
 	<meta property="og:url" content="https://blog-red-nine-44.vercel.app" />
